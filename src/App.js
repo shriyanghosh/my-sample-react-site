790b6348f9cdf060a7c5f02867cae67cb6efeb64
Swaroop <swaroop@ip-10-212-128-22.ap-south-1.compute.internal>
2024-12-18 16:57:37 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Sample React Website</h1>
+        <p>This is a simple React app to get started.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
