d143a21cb0cb61ae86742a9cc364bcc8cd7154f6
Swaroop <swaroop@ip-10-212-128-51.ap-south-1.compute.internal>
2024-12-20 17:58:56 +0530

    Added CNAMEs

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to My Sample React Website</h1>
+        <h1>Welcome to My Sample React Website - Swaroop Ghosh</h1>
         <p>This is a simple React app to get started.</p>
       </header>
     </div>
